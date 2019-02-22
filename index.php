e00ed1ede8ee1ac2354d68f8a73952d7f84ba39c
maxbemol <maxverville14@gmail.com>
2019-02-22 09:21:07 -0500

    Ajout des fichiers de base

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="src/nmgs.css" />
+        <title>NM-GeekShow</title>
+    </head>
+
+    <body>
+        <header>
+
+        </header>
+
+            <nav>
+                <?php include("menu_base.php"); ?>
+            </nav>
+        
+            <section>
+                <aside>
+
+                </aside>
+                <article>
+
+                </article>  
+            </section>
+        
+        <footer>
+
+        </footer>
+    </body>
+    
+</html>
